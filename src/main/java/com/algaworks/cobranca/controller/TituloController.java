b0a3f7f4370b63e0a16186778e5a8a63547fa406
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-01-15 17:13:32 -0200

    Protótipo de pesquisa de títulos

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -39,6 +39,12 @@ public class TituloController {
 		return mv;
 	}
 	
+	@RequestMapping
+	public String pesquisar()
+	{
+		return "PesquisaTitulos";
+	}
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo()
 	{
